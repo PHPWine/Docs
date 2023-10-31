cc1b55fe3fcd9f4232bf66aebe679e9436dd3a43
nielsoffice <nieldigitalsolution@gmail.com>
2023-10-31 20:56:15 +0800

    up

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php require __DIR__ . '/library/PHPAutoloader/PHPAutoloader.php'; 
+
+ use \PHPAutoloader\Wine\RequestClass;
+
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -18,6 +24,14 @@
         <![endif]-->
         <h1>PHPWine Documments</h1>
          <a href="./about.php">About</a>
+
+         <?php 
+         
+         $RequestClass = new RequestClass();
+         echo "<br />";
+         echo  $RequestClass->getRequestClass();
+
+         ?>
         
         <script src="dist/bundle.caeffae0ceda09a9d4b0.js" async defer></script>
     </body>
---
+++ library/PHPAutoloader/PHPAutoloader.php
@@ -0,0 +1,72 @@
+<?php 
+
+/**
+ * @copyright (c) 2021 PHP Autoloader v1.0 Cooked by nielsoffice 
+ *
+ * MIT License
+ *
+ * PHP Authentication (effortless) v1.0 free software: you can redistribute it and/or modify.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @category   PHP Classes autoloader
+ * @package    namespace, autoloader, spl
+ *            
+ *            
+ * @author    nielfernandez <nielsoffice.wordpress.php@gmail.com>
+ * @license   MIT License
+ * @link      https://github.com/nielsofficeofficial/PHPAuth-Effortless
+ * @link      https://github.com/nielsofficeofficial/PHPAuth-Effortless/blob/main/README.md
+ * @link      https://www.facebook.com/nielsofficeofficial
+ * @version   v1.0
+ *
+ */
+
+    $PHPAutoLoader = new class {
+ 
+      public function __construct() 
+       {
+
+        define('AUTH_DIRECTORY_FILE_REQUEST','PHPAutoloader\\Wine\\');
+
+        define('BASE_DIR_SYSTEM_FILE', dirname( dirname( __DIR__ ) ) .'/views/' );
+       
+        define('_PHP_','.php');
+    
+            spl_autoload_register(function($class) 
+            {
+            
+                // File name space
+                $systemFileRequest = AUTH_DIRECTORY_FILE_REQUEST;
+            
+                // base directory system file
+                $sourceFileRquest  = BASE_DIR_SYSTEM_FILE;                
+                if (strncmp($systemFileRequest, $class, strlen($systemFileRequest)) !== 0) return;
+            
+                // Directory system data files
+                $dataRequest = $sourceFileRquest . str_replace('\\', '/', substr($class, strlen($systemFileRequest))) . _PHP_;
+            
+                // if the file exists, require it
+                (file_exists($dataRequest)) ? require $dataRequest : false;
+            
+            });
+    
+       }
+   };
+    
+ 
